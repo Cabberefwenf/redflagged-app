1ca3d7f575f710148ade491dee61603d22ecc3fb
Christopher <cazar-brandes@outlook.com>
2025-07-07 13:58:38 -0400

    Create index.tsx

---
+++ src/router/index.tsx
@@ -0,0 +1,19 @@
+// src/router/index.tsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../App";
+import Menu from "../pages/Menu";
+import ShareExperience from "../pages/ShareExperience";
+import ExperienceFeed from "../pages/ExperienceFeed";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/share" element={<ShareExperience />} />
+        <Route path="/feed" element={<ExperienceFeed />} />
+      </Routes>
+    </Router>
+  );
+}
