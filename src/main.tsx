86d2eda1b8ad770a0edd740ecb1d56f71c2e0f1d
Christopher <cazar-brandes@outlook.com>
2025-07-07 15:15:19 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import './styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/index.css';
 import AppRouter from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
