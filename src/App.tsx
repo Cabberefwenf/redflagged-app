802ae91ade7f9450adcdaca04461a2b4c39e6df5
Christopher <cazar-brandes@outlook.com>
2025-07-07 16:48:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ExperienceFeed from './pages/ExperienceFeed';
-import ShareExperience from './pages/ShareExperience';
-import Menu from './pages/Menu';
-import PremiumModal from './components/PremiumModal';
+import AppRouter from "./router";
 
-const App = () => {
-  return (
-    <div className="bg-backgroundDark min-h-screen text-white">
-      <Routes>
-        <Route path="/" element={<ExperienceFeed />} />
-        <Route path="/submit" element={<ShareExperience />} />
-        <Route path="/menu" element={<Menu />} />
-      </Routes>
-
-      {/* Global Premium Modal Logic (stub) */}
-      <PremiumModal />
-    </div>
-  );
-};
-
-export default App;
+export default function App() {
+  return <AppRouter />;
+}
